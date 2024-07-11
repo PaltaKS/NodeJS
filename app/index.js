1341fb8935cf0ea73fccfd3a2ea1c799df29bd98
unknown <bpavez03@gmail.com>
2024-07-11 09:34:58 -0400

    mi primer commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+const express=require("express");
+
+//server
+
+const app = express();
+app.set("port",4000)
+app.listen(app.get("port"));
+console.log("Servidor corriendo en puerto", app.get("port"));
+
+
+//Rutas
+
+app.get("/",(req,res)=> res.sendFile(__dirname + "/pages/login.html"))
+
+
+
