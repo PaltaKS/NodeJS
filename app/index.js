ac3ed8770eed1ef9f2038650e96772c1ed837a1c
Bastian Pavez <bpavez03@gmail.com>
2024-07-13 04:30:21 -0400

    pruebas

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
-const express=require("express");
+ const express = require("express");
 
-//Server
-
-const app = express();
-app.set("port",4000)
-app.listen(app.get("port"));
-console.log("Servidor corriendo en puerto", app.get("port"));
-
-
-//Rutas
-
-app.get("/",(req,res)=> res.sendFile(__dirname + "/pages/login.html"))
+ //Server
+ 
+ const app = express();
+ app.set("port",4000)
+ app.listen(app.get("port"));
+ console.log("Servidor corriendo en puerto", app.get("port"));
+ 
+ 
+ //Rutas
+ 
+ app.get("/",(req,res)=> res.sendFile(__dirname + "/pages/login.html"))
